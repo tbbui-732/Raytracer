1323bb006dc359416e890bfbe71d232867df1ad1
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-12-17 23:19:53 -0500

    Add color utility function

--- rtiow.c
+++ rtiow.c
@@ -5,13 +5,15 @@
 #define PROJECT_NAME "RTIOW"
 #define EPSILON 1e-9
 
+/*****************/
 /* START OF VEC3 */
+/*****************/
 typedef struct {
     double x, y, z;
 } vec3;
 
 void print(const vec3* Vector) {
-    printf("%f %f %f\n", Vector->x, Vector->y, Vector->z);
+    fprintf(stdout, "%f %f %f\n", Vector->x, Vector->y, Vector->z);
 }
 
 vec3 add(const vec3* self, const vec3* other) {
@@ -80,6 +82,28 @@ vec3 unit(const vec3* Vector) {
 }
 /* END OF VEC3 */
 
+
+/****************************/
+/* START OF COLOR UTILITIES */
+/****************************/
+typedef vec3 color;
+
+void write_color(const color* pixel_color) {
+    double r = pixel_color->x;
+    double g = pixel_color->y;
+    double b = pixel_color->z;
+
+    // translate [0,1] components to a byte range [0, 255]
+    int rbyte = (int)(255.999 * r);
+    int gbyte = (int)(255.999 * g);
+    int bbyte = (int)(255.999 * b);
+
+    fprintf(stdout, "%d %d %d\n", rbyte, gbyte, bbyte);
+}
+
+/* END OF COLOR UTILITIES */
+
+
 int main(void) {
     const int width = 256;
     const int height = 256;
@@ -87,23 +111,19 @@ int main(void) {
     FILE* file = fopen("image.ppm", "w");
 
     if (file == NULL) {
-        printf("Error opening file: %s\n", "image.ppm");
+        fprintf(stderr, "Error opening file: %s\n", "image.ppm");
         return EXIT_FAILURE;
     }
 
-    fprintf(file, "P3\n%d %d\n255\n", width, height);
-
-    for (int i = 0; i < height; ++i) {
-        for (int j = 0; j < width; ++j) {
-            double r = (double)(i) / (width-1);
-            double g = (double)(j) / (height - 1);
-            double b = 0.0;
-
-            int ir = (int)(255.999 * r);
-            int ig = (int)(255.999 * g);
-            int ib = (int)(255.999 * b);
-
-            fprintf(file, "%d %d %d\n", ir, ig, ib);
+    fprintf(stdout, "P3\n%d %d\n255\n", width, height);
+    for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+            color pixel_color = {
+                (double)(i) / (width-1),
+                (double)(j) / (height-1),
+                (double)(i) / (width-1)
+            };
+            write_color(&pixel_color);
         }
     }
 
