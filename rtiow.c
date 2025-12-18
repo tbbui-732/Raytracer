b57058217855b1afcdbfdae535108500c976157f
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-12-18 01:45:27 -0500

    Update viewport

--- rtiow.c
+++ rtiow.c
@@ -11,8 +11,6 @@
 typedef struct {
     double x, y, z;
 } vec3;
-typedef vec3 point;
-typedef vec3 color;
 
 void print(const vec3* Vector) {
     fprintf(stdout, "%f %f %f\n", Vector->x, Vector->y, Vector->z);
@@ -88,7 +86,7 @@ vec3 unit(const vec3* Vector) {
 /****************************/
 /* START OF COLOR UTILITIES */
 /****************************/
-void write_color(const color* pixel_color) {
+void write_color(const vec3* pixel_color) {
     double r = pixel_color->x;
     double g = pixel_color->y;
     double b = pixel_color->z;
@@ -106,19 +104,52 @@ void write_color(const color* pixel_color) {
 /* START OF RAY/RAYS */
 /*********************/
 typedef struct {
-    point origin;
+    vec3 origin;
     vec3 direction;
 } ray;
 
-point at(const ray* r, const double t) {
-    return r->origin + (r->direction * t);
+vec3 at(const ray* r, const double t) {
+    return add(r->origin, multiply(&(r->direction), t));
 }
 
+vec3 ray_color(const ray* r) {
+    vec3 new = {0, 0, 0};
+    return new;
+}
 /* END OF RAY/RAYS */
 
 int main(void) {
-    const int width = 256;
-    const int height = 256;
+    const double aspect_ratio = 16.0 / 9.0;
+
+    // calculate screen width and height, ensuring the height is atleast 1
+    int image_width = 400;
+    int image_height = (int)(image_width / aspect_ratio);
+    if (image_height < 1) image_height = 1;
+
+    // camera
+    double focal_length = 1.0;
+    vec3 camera_center = {0};
+
+    // viewport
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height *
+                            (double)image_width / (double)image_height;
+
+    // vectors across horizontal and vertical viewport edge
+    vec3 viewport_u = {viewport_width, 0, 0};
+    vec3 viewport_v = {0, -viewport_height, 0};
+
+    // horizontal and vertical delta vectors from pixel to pixel
+    vec3 pixel_delta_u = divide(viewport_u, width);
+
+    // Calculate the location of the upper left pixel
+    vec3 viewport_upper_left = camera_center -
+                                (vec3){0, 0, focal_length} -
+                                divide(viewport_u, 2) -
+                                divide(viewport_u, 2);
+
+    vec3 pixel00_location = viewport_upper_left + 0.5 *
+                             add(pixel_delta_u, pixel_delta_v);
 
     FILE* file = fopen("image.ppm", "w");
 
@@ -130,11 +161,11 @@ int main(void) {
     fprintf(stdout, "P3\n%d %d\n255\n", width, height);
     for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
-            color pixel_color = {
-                (double)(i) / (width-1),
-                (double)(j) / (height-1),
-                (double)(i) / (width-1)
-            };
+            vec3 pixel_center = pixel00_location +
+                                 (i * pixel_delta_u) + (j * pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
+            ray r = {camera_center, ray_direction};
+            vec3 pixel_color = ray_color(&r);
             write_color(&pixel_color);
         }
     }
