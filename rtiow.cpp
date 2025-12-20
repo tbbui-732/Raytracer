bd5d074e8d8e8b5952434d03dbb0047208ccb443
Bao Bui <80117447+tbbui-732@users.noreply.github.com>
2025-12-20 08:34:55 -0500

    Complete rewrite of operator overloads

--- rtiow.cpp
+++ rtiow.cpp
@@ -22,33 +22,34 @@ struct vec3 {
     f64 x, y, z;
 
     vec3() { x = 0.; y = 0.; z = 0.; }
-    vec3(double _x, double _y, double _z) : x(_y), y(_y), z(_z) {}
+    vec3(const f64 _x, const f64 _y, const f64 _z) : x(_x), y(_y), z(_z) {}
+    vec3(const vec3* other) : x(other->x), y(other->y), z(other->z) {}
 
     // additional accessors for colors
-    double r() { return x; }
-    double g() { return y; }
-    double b() { return z; }
+    f64 r() { return x; }
+    f64 g() { return y; }
+    f64 b() { return z; }
 
-    void print() { std::cout << x << ' ' << y << ' ' << z << '\n' }
+    void print() { std::cout << x << ' ' << y << ' ' << z << '\n'; }
 
     // arithmetic
-    vec3 operator+(const vec3* other) {
-        return = vec3(
-            this->x + other->x,
-            this->y + other->y,
-            this->z + other->z
-        );
-    }
-
-    vec3 operator-(const vec3* other) {
+    vec3 operator+(const vec3 other) const {
         return vec3(
-            this->x - other->x,
-            this->y - other->y,
-            this->z - other->z
+            this->x + other.x,
+            this->y + other.y,
+            this->z + other.z
         );
     }
 
-    vec3 operator*(const double scalar) {
+    vec3 operator-(const vec3 other) const {
+        return vec3(
+            this->x - other.x,
+            this->y - other.y,
+            this->z - other.z
+        );
+    }
+
+    vec3 operator*(const f64 scalar) const {
         return vec3(
             this->x * scalar,
             this->y * scalar,
@@ -56,7 +57,7 @@ struct vec3 {
         );
     }
 
-    vec3 operator/(const double scalar) {
+    vec3 operator/(const f64 scalar) const {
         return vec3(
             this->x / scalar,
             this->y / scalar,
@@ -64,65 +65,73 @@ struct vec3 {
         );
     }
 
-    void operator+=(const vec3* other) {
-        x += other->x;
-        y += other->y;
-        z += other->z;
+    void operator+=(const vec3 other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
     }
 
-    void operator-=(const vec3* other) {
-        x -= other->x;
-        y -= other->y;
-        z -= other->z;
+    void operator-=(const vec3 other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
     }
 
-    void operator*=(const double scalar) {
+    void operator*=(const f64 scalar) {
         x *= scalar;
         y *= scalar;
         z *= scalar;
     }
 
-    void operator/=(const double scalar) {
+    void operator/=(const f64 scalar) {
         x /= scalar;
         y /= scalar;
         z /= scalar;
     }
 
-    double length_squared() {
-        double x_squared = pow(this->x, 2);
-        double y_squared = pow(this->y, 2);
-        double z_squared = pow(this->z, 2);
+    f64 length_squared() const {
+        f64 x_squared = pow(this->x, 2);
+        f64 y_squared = pow(this->y, 2);
+        f64 z_squared = pow(this->z, 2);
         return x_squared + y_squared + z_squared;
     }
 
-    double length() {
+    f64 length() const {
         return sqrt(this->length_squared());
     }
 
-    double magnitude() {
+    f64 magnitude() const {
         return this->length();
     }
 
-    double norm() {
+    f64 norm() const {
         return this->length();
     }
 
-    vec3 unit() {
-        return this / this->length();
+    vec3 unit() const {
+        return *this / this->length();
     }
 };
 
-double dot(const vec3* self, const vec3* other) {
-    return self->x * other->x +
-           self->y * other->y +
-           self->z * other->z;
+vec3 operator*(const f64 scalar, const vec3 vector) {
+    return vector * scalar;
 }
 
-vec3 cross(const vec3* self, const vec3* other) {
+vec3 operator/(const f64 scalar, const vec3 vector) {
+    return vector / scalar;
+}
+
+f64 dot(const vec3 self, const vec3 other) {
+    return (self.x * other.x) +
+           (self.y * other.y) +
+           (self.z * other.z);
+}
+
+vec3 cross(const vec3 self, const vec3 other) {
     return vec3(
-        self->y * other->z - self->z * other->y,
-        self->z * other->x - self->x * other->z,
-        self->x * other->y - self->y * other->x
+        (self.y * other.z) - (self.z * other.y),
+        (self.z * other.x) - (self.x * other.z),
+        (self.x * other.y) - (self.y * other.x)
     );
 }
 
@@ -132,17 +141,17 @@ vec3 cross(const vec3* self, const vec3* other) {
 /****************************/
 /* START OF COLOR UTILITIES */
 /****************************/
-void write_color(const vec3* pixel_color) {
-    double r = pixel_color->x;
-    double g = pixel_color->y;
-    double b = pixel_color->z;
+void write_color(const vec3 pixel_color) {
+    f64 r = pixel_color.x;
+    f64 g = pixel_color.y;
+    f64 b = pixel_color.z;
 
     // translate [0,1] components to a byte range [0, 255]
-    int rbyte = (int)(255.999 * r);
-    int gbyte = (int)(255.999 * g);
-    int bbyte = (int)(255.999 * b);
+    u32 rbyte = (u32)(255.999 * r);
+    u32 gbyte = (u32)(255.999 * g);
+    u32 bbyte = (u32)(255.999 * b);
 
-    fprintf(stdout, "%d %d %d\n", rbyte, gbyte, bbyte);
+    std::cout << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
 /* END OF COLOR UTILITIES */
 
@@ -150,89 +159,80 @@ void write_color(const vec3* pixel_color) {
 /* START OF RAY/RAYS */
 /*********************/
 struct ray {
-    vec3 origin;
-    vec3 direction;
+    vec3 origin, direction;
+
+    ray() {
+        origin = vec3();
+        direction = vec3();
+    }
+
+    ray(const vec3 _origin, const vec3 _direction) {
+        origin = vec3(_origin);
+        direction = vec3(_direction);
+    }
 };
 
-vec3 at(const ray* r, const double t) {
-    /*
-        P(t) = A + t*b;
-        s.t. `A` is the origin, `b` is the direction, and `t` is real-value
-    */
-    vec3 t_times_b = multiply(&r->direction, t);
-    return add(&r->origin, &t_times_b);
+/*
+ * P(t) = A + t*b;
+ * P(t) = Point on a line/ray
+ * `A` = Origin -> typically the camera position
+ * `b` = Direction
+ * `t` = Some real value that dictates how far you move along `b`
+ */
+vec3 at(const ray r, const double t) {
+    return r.origin + (r.direction * t);
 }
 
-vec3 ray_color(const ray* r) {
-    vec3 unit_direction = unit(&r->direction);
-    double a = 0.5 * (unit_direction.y + 1.0);
-
+vec3 ray_color(const ray r) {
+    vec3 unit_direction = r.direction.unit();
+    f64 a = 0.5 * (unit_direction.y + 1.0);
     vec3 color1 = {1.0, 1.0, 1.0};
     vec3 color2 = {0.5, 0.7, 1.0};
-
-    vec3 op1 = multiply(&color1, (1.0-a));
-    vec3 op2 = multiply(&color2, a);
-    vec3 result = add(&op1, &op2);
-    return result;
+    return (color1 * (1.0-a)) + (color2 * a);
 }
 /* END OF RAY/RAYS */
 
 int main(void) {
-    const double aspect_ratio = 16.0 / 9.0;
+    // Image
 
-    // calculate screen width and height, ensuring the height is atleast 1
+    auto aspect_ratio = 16.0 / 9.0;
     int image_width = 400;
-    int image_height = (int)(image_width / aspect_ratio);
-    if (image_height < 1) image_height = 1;
 
-    // camera
-    double focal_length = 1.0;
-    vec3 camera_center = {0};
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
 
-    // viewport
-    double viewport_height = 2.0;
-    double viewport_width = viewport_height *
-                            (double)image_width / (double)image_height;
+    // Camera
 
-    // vectors across horizontal and vertical viewport edge
-    vec3 viewport_u = {viewport_width, 0, 0};
-    vec3 viewport_v = {0, -viewport_height, 0};
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    auto camera_center = vec3(0, 0, 0);
 
-    // horizontal and vertical delta vectors from pixel to pixel
-    vec3 pixel_delta_u = divide(&viewport_u, (double)image_width);
-    vec3 pixel_delta_v = divide(&viewport_v, (double)image_height);
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
 
-    // Calculate the location of the upper left pixel
-    vec3 viewport_u_half = divide(&viewport_u, 2);
-    vec3 viewport_v_half = divide(&viewport_v, 2);
-    vec3 viewport_dif = sub(&viewport_u_half, &viewport_v_half);
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
 
-    vec3 viewport_upper_left = sub(&camera_center, &(vec3){0, 0, focal_length});
-    viewport_upper_left = sub(&viewport_upper_left, &viewport_dif);
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center
+        - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-    vec3 pixel00_location = add(&pixel_delta_u, &pixel_delta_v);
-    pixel00_location = multiply(&pixel00_location, 0.5);
-    pixel00_location = add(&viewport_upper_left, &pixel00_location);
+    // Render
 
-    if (file == nullptr) {
-        std::cerr << "Error opening files: image.ppm\n";
-        return EXIT_FAILURE;
-    }
-
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n"
+    std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
     for (int j = 0; j < image_height; j++) {
         for (int i = 0; i < image_width; i++) {
-            vec3 iu = multiply(&pixel_delta_u, i);
-            vec3 jv = multiply(&pixel_delta_v, j);
-            vec3 pixel_center = add(&iu, &jv);
-            pixel_center = add(&pixel_center, &pixel00_location);
+            auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            auto ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
 
-            vec3 ray_direction = sub(&pixel_center, &camera_center);
-            ray r = {camera_center, ray_direction};
-            vec3 pixel_color = ray_color(&r);
-            write_color(&pixel_color);
+            vec3 pixel_color = ray_color(r);
+            write_color(pixel_color);
         }
     }
-
-    return EXIT_SUCCESS;
 }
